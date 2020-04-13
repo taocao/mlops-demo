5ff3451fda1b2de3851c1e0b47cc40578052106c
Ben Keen <bekeen@microsoft.com>
2020-04-13 21:13:25 +0100

    Update score.py to use model name as constant

--- src/my_custom_package/score.py
+++ src/my_custom_package/score.py
@@ -4,10 +4,12 @@ import joblib
 import numpy as np
 from azureml.core.model import Model
 
+from my_custom_package.utils.const import MODEL_NAME
+
 
 def init():
     global model
-    model_path = Model.get_model_path('binary_classifier')
+    model_path = Model.get_model_path(MODEL_NAME)
     model = joblib.load(model_path)
 
 
